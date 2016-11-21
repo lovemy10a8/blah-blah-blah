68ce4d0709a15f6cedee76d12bae30599e152cbc
LonelyRoader <anhratthichquay123@yahoo.com.vn>
2016-11-21 15:38:06 +0000

    Update Main.php

--- src/Main/Main.php
+++ src/Main/Main.php
@@ -4,6 +4,8 @@ namespace Main;
 
 use pocketmine\plugin\PluginBase;
 use pocketmine\event\Listener;
+use pocketmine\command\Command;
+use pocketmine\command\CommandSender;
 
 class Main extends PluginBase {
   public function onCommand(CommandSender $s, Command $cmd, $label, array $args) {

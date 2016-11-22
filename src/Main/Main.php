082c48fa005e00cc61a355d85d3357cf21be03f9
LonelyRoader <anhratthichquay123@yahoo.com.vn>
2016-11-22 12:27:12 +0000

    Update Main.php

--- src/Main/Main.php
+++ src/Main/Main.php
@@ -11,6 +11,7 @@ class Main extends PluginBase {
   public function onCommand(CommandSender $s, Command $cmd, $label, array $args) {
       if($cmd->getName() == "batdau") {
     $this->getServer()->getScheduler()->scheduleRepeatingTask(new MainTask($this), 20);
+        return true;
 }
 }
 }

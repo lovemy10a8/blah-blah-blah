fe32cf828126bf4d978c2241411adf10f6ad302b
LonelyRoader <anhratthichquay123@yahoo.com.vn>
2016-11-23 14:21:26 +0000

    Update MainTask.php

--- src/Main/MainTask.php
+++ src/Main/MainTask.php
@@ -10,49 +10,43 @@ use pocketmine\command\CommandSender;
 class MainTask extends PluginTask {
 
 	public $seccond = 10;
-
+	
 	public function onRun($tick) {
 		$this->seccond--;
 		if($this->seccond == 5) {
 			foreach($this->getOwner()->getServer()->getOnlinePlayers() as $p) {
-
 			$p->sendMessage("Còn 5 giây");
  		}
 	}
 
 	if($this->seccond == 4) {
 		foreach($this->getOwner()->getServer()->getOnlinePlayers() as $p) {
-
 		$p->sendMessage("Còn 4 giây");
 		}
 	}
 
-if($this->seccond == 3) {
-foreach($this->getOwner()->getServer()->getOnlinePlayers() as $p) {
+	if($this->seccond == 3) {
+		foreach($this->getOwner()->getServer()->getOnlinePlayers() as $p) {
+		$p->sendMessage("Còn 3 giây");
+		}
+	}
 
-$p->sendMessage("Còn 3 giây");
-}
-}
+	if($this->seccond == 2) {
+		foreach($this->getOwner()->getServer()->getOnlinePlayers() as $p) {
+		$p->sendMessage("Còn 2 giây");
+		}
+	}
+	
+	if($this->seccond == 1) {
+		foreach($this->getOwner()->getServer()->getOnlinePlayers() as $p) {
+		$p->sendMessage("Còn 1 giây");
+		}
+	}
 
-if($this->seccond == 2) {
-foreach($this->getOwner()->getServer()->getOnlinePlayers() as $p) {
-
-$p->sendMessage("Còn 2 giây");
-}
-}
-
-if($this->seccond == 1) {
-foreach($this->getOwner()->getServer()->getOnlinePlayers() as $p) {
-
-$p->sendMessage("Còn 1 giây");
-}
-}
-
-if($this->seccond == 0) {
-foreach($this->getOwner()->getServer()->getOnlinePlayers() as $p) {
-
-$this->getOwner()->getServer()->getScheduler()->cancelTask($this->getTaskId());
-}
+	if($this->seccond == 0) {
+		foreach($this->getOwner()->getServer()->getOnlinePlayers() as $p) {
+		$this->getOwner()->getServer()->getScheduler()->cancelTask($this->getTaskId());
+	}
 }
 }
 }

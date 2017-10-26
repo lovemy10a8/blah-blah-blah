daa5f7ff926621f8a214595eed996782d8275079
XEmAX32 <XEmAX32@gmail.com>
2017-10-26 21:36:54 +0200

    Update Main.php

--- src/Main/Main.php
+++ src/Main/Main.php
@@ -10,8 +10,9 @@ use pocketmine\command\CommandSender;
 class Main extends PluginBase {
   public function onCommand(CommandSender $s, Command $cmd, $label, array $args) {
       if($cmd->getName() == "batdau") {
-    $this->getServer()->getScheduler()->scheduleRepeatingTask(new MainTask($this), 20);
-        return true;
+        $this->getServer()->getScheduler()->scheduleRepeatingTask(new MainTask($this), 20);
+      return true;
+      }
+  }
 }
-}
-}
+?>
